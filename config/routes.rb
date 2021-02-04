8746da0e5e0b34db5e7bf71386eec271f1bc0e09
kubomi-3 <yfhyfh0@gmail.com>
2021-02-04 14:44:49 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'subjects#index'
+  resources :subjects, only: [:index, :new, :create, :destroy, :show] do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
+  post '/subjects/:id/vote1', to: 'subjects#vote1'
+  post '/subjects/:id/vote2', to: 'subjects#vote2'
+  post '/subjects/:id/vote3', to: 'subjects#vote3'
 end
