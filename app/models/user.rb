67771faf91f8951ba6c336dc6c0e464832527c1a
kubomi-3 <yfhyfh0@gmail.com>
2021-02-04 14:23:20 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  has_many :subjects
+  has_many :comments
+  has_many :ans_1s 
+  has_many :ans_2s
+  has_many :ans_3s
+
 end
